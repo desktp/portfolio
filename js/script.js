7de4f7bb699846ff8592234181dcea0396626451
desktp <j.mouette@gmail.cOM>
2014-09-09 15:11:33 -0300

    javascript
    transições javascript funcionando, titulos dos botoes funcionando, logo
    refeito

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-function OnHover (divId){
-  this.style.backgroundImage = "url(img/buttons/"+this.id+"-hover.gif";
-}
-
-function OffHover (divId){
-}
-
 function addEvent(evnt, elem, func) {
   if (elem.addEventListener)  // W3C DOM
     elem.addEventListener(evnt,func,false);
@@ -16,30 +9,21 @@ function addEvent(evnt, elem, func) {
   }
 }
 
-function removeClass(el, className){
-  el.className = el.className.replace(className, "");
+function removeClass(elem, className){
+  elem.className = elem.className.replace(className, "");
 }
 
-function addClass(el, className){
-  el.className = el.className + " " +  className;
+function addClass(elem, className){
+  elem.className = elem.className + " " +  className;
 }
 
 function onclick(evt){
-  evt.preventDefault();
-  ChangeActiveFrame(this.hash.substring(1));
+  if(this.href.substring(0,4) != 'http')
+  {
+    evt.preventDefault();
+    ChangeActiveFrame(this.hash.substring(1));
+  } 
 }
-
-var anchors = document.getElementsByTagName('a');
-
-for(var i in anchors){
-  if(!anchors[i]['href']) continue;
-  addEvent("click", anchors[i], onclick);
-  addEvent("mouseover", anchors[i], OnHover);
-  addEvent("mouseout", anchors[i], OffHover);
-}
-
-var body = document.getElementsByTagName('body');
-addEvent("hashchange", body, onhashchange)
 
 function ChangeActiveFrame(activeDiv){
   var activeButton = activeDiv.substring(8);
@@ -56,11 +40,21 @@ function ChangeActiveFrame(activeDiv){
   for (var i =otherButtons.length - 1; i >= 0; i--)
   {
     otherButtons[i].style.backgroundImage = "url('img/buttons/"+otherButtons[i].id+"-default.gif')";
+    document.getElementById(otherButtons[i].id + '-title').style.top = '100px';
   };
 
   if(activeButton != ""){
     buttonToChange.style.backgroundImage = "url('img/buttons/"+buttonToChange.id+"-active.gif')";
 
     addClass(document.getElementById(activeDiv), "show");
+    document.getElementById(activeButton + '-title').style.top = '70px';
   }
 }
+
+var anchors = document.getElementsByTagName('a');
+
+for(var i in anchors){
+  if(!anchors[i]['href']) continue;
+  addEvent("click", anchors[i], onclick);
+}
+
